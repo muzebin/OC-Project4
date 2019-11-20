e38ccff7852b1bb78917ca7eb6535b3e9af08087
Jérôme Guèrin <guerinjerome@outlook.fr>
2019-11-20 20:30:01 +0100

    action "layout picture" finished

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonLayoutOne: UIButton!
+    @IBOutlet weak var buttonLayoutTwo: UIButton!
+    @IBOutlet weak var buttonLayoutThree: UIButton!
+    @IBOutlet weak var buttonTopLeft: UIButton!
+    @IBOutlet weak var buttonTopRight: UIButton!
+    @IBOutlet weak var buttonBottomLeft: UIButton!
+    @IBOutlet weak var buttonBottomRight: UIButton!
+    
+    @IBAction func tapLayoutOne() {
+        buttonTopLeft.isHidden = true
+        buttonTopRight.isHidden = false
+        buttonBottomLeft.isHidden = false
+        buttonBottomRight.isHidden = false
+        buttonLayoutOne.setImage(UIImage(named: "Selected"), for: .normal)
+        buttonLayoutTwo.setImage(nil, for: .normal)
+        buttonLayoutThree.setImage(nil, for: .normal)
+    }
+    
+    @IBAction func tapLayoutTwo() {
+        buttonTopLeft.isHidden = false
+        buttonTopRight.isHidden = false
+        buttonBottomLeft.isHidden = true
+        buttonBottomRight.isHidden = false
+        buttonLayoutOne.setImage(nil, for: .normal)
+        buttonLayoutTwo.setImage(UIImage(named: "Selected"), for: .normal)
+        buttonLayoutThree.setImage(nil, for: .normal)
+    }
+    
+    @IBAction func tapLayoutThree() {
+        buttonTopLeft.isHidden = false
+        buttonTopRight.isHidden = false
+        buttonBottomLeft.isHidden = false
+        buttonBottomRight.isHidden = false
+        buttonLayoutOne.setImage(nil, for: .normal)
+        buttonLayoutTwo.setImage(nil, for: .normal)
+        buttonLayoutThree.setImage(UIImage(named: "Selected"), for: .normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tapLayoutTwo()
     }
-
-
 }
 
